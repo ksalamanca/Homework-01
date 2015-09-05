c454fe4bab1cad1ada2f366b4bd53f7196ceb87e
ksalamanca <ksalamanca@cca.edu>
2015-09-05 15:22:09 -0700

    Create Puzzle-06.js

---
+++ Puzzle-06.js
@@ -0,0 +1,19 @@
+right();
+if (getColor()=="red") {
+    up();
+		up();
+  	up();
+} else {
+  down();
+  down();
+  down();
+}
+if (getColor()=="blue") {
+  right();
+  up();
+  down();
+} else {
+  left();
+  up();
+  down();
+}
